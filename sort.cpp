9290cb636c272982feb875107cea7c4fc2be3a87
mtuck <Mtuck@una.edu>
2014-12-07 13:46:06 -0600

    everything but matrix

--- sort.cpp
+++ sort.cpp
@@ -21,6 +21,12 @@ void bubbleSort(T a[], int size, int & assignments, int & comparisons);
 template <class T>
 void selectionSort(T arr[], int size, int & assignments, int & comparisons);
 
+template <class T>
+void quicksort(T arr[], int size, int & assignments, int & comparisons);
+template <class T>
+void quicksort(T arr[], int left, int right, int & assignments, int & comparisons);
+template <class T>
+const T & median3(T arr[], int left, int right, int & assignments, int & comparisons);
 
 int main(){
 
@@ -57,22 +63,20 @@ int main(){
 		cout<<endl;
 		switch(choice){
 			case 1: printArray(arr,size); break;
-			case 2: bubbleSort(arr,size,assignments,comparisons); break;
-			case 3: selectionSort(arr,size,assignments,comparisons); break;
-			case 4: mergeSort(arr,size,assignments,comparisons); break;
-			case 5: cout<<"Quicksort is not completed"; break;
+			case 2: bubbleSort(arr,size,assignments,comparisons); cout<<"Array sorted in "<<assignments<<" assignments and "<<comparisons<<" comparisons";  break;
+			case 3: selectionSort(arr,size,assignments,comparisons); cout<<"Array sorted in "<<assignments<<" assignments and "<<comparisons<<" comparisons";  break;
+			case 4: mergeSort(arr,size,assignments,comparisons);  cout<<"Array sorted in "<<assignments<<" assignments and "<<comparisons<<" comparisons";  break;
+			case 5: quicksort(arr,size,assignments,comparisons);  cout<<"Array sorted in "<<assignments<<" assignments and "<<comparisons<<" comparisons";  break;
 			case 6: FillOrdered(arr,size); break;
 			case 7: FillReverseOrder(arr,size); break;
 			case 8: FillRandom(arr, size); break;
 			case 9: break;
 		}
 	} 
-	
-	
 	delete [] arr;
-
 	return 0;
 }
+
 
 void FillRandom(int arr[], int size){
 	for(int i = 0; i<size; i++)
@@ -117,8 +121,10 @@ void bubbleSort(T a[],int size, int & assignments, int & comparisons){
 
 template <class T>
 void printArray(T arr[], int size){
+	cout<<"\n--------------------------------------------------------------------------\n";
 	for(int i = 0; i < size; i++)
 		cout<<arr[i]<<"\t";
+	cout<<"\n--------------------------------------------------------------------------\n";
 }
 
 template <class T>
@@ -137,13 +143,15 @@ void selectionSort(T arr[], int size, int & assignments, int & comparisons){
 				minIndex = j;			assignments++;
 			}
 		}
-		swap(arr[i], arr[minIndex]);   assignements+=3;
+		swap(arr[i], arr[minIndex]);   assignments+=3;
 	}
 }
 
 
 template <class T>
 void mergeSort(T arr[], int size, int & assignments, int & comparisons){
+	assignments = 0;
+	comparisons = 0;
 	T* tmp = new T[size];
 	mergeSort(arr, tmp, 0, size-1,assignments,comparisons);
 	delete tmp;
@@ -153,7 +161,7 @@ void mergeSort(T arr[], int size, int & assignments, int & comparisons){
 template <class T>
 void mergeSort(T arr[], T tmp[], int left, int right, int & assignments, int & comparisons){
 	if(left < right){
-		int middle = (left + right,assignments,comparisons)/2;
+		int middle = (left + right)/2; assignments++;
 		mergeSort(arr,tmp,left,middle,assignments,comparisons);
 		mergeSort(arr,tmp,middle+1,right,assignments,comparisons);
 		merge(arr, tmp, left, middle+1,right,assignments,comparisons);
@@ -162,28 +170,88 @@ void mergeSort(T arr[], T tmp[], int left, int right, int & assignments, int & c
 
 template <class T>
 void merge(T arr[], T tmp[], int left, int right, int end, int & assignments, int & comparisons){
-	int leftEnd = right-1;
-	int curr = left;
-	int size = end - left +1;
+	int leftEnd = right-1; 		assignments++;
+	int curr = left;			assignments++;
+	int size = end - left +1; 	assignments++;
 	
-	
+	comparisons+=2;
 	while(left <= leftEnd && right <= end){
 		if(arr[left] <= arr[right])
 			tmp[curr++] = arr[left++];      //get the smallest item of the two sorted arrays
 		else
 			tmp[curr++] = arr[right++];
+		assignments++;  //exactly one assignment per iteration
+		comparisons+=2;
 	}
 	
-	while(left <= leftEnd)
-		tmp[curr++] = arr[left++];
-	
-	while(right <= end)
-		tmp[curr++] = arr[right++];
-	
+	comparisons++;
+	while(left <= leftEnd){
+		tmp[curr++] = arr[left++]; assignments++; comparisons++;
+	}
+	comparisons++;
+	while(right <= end){
+		tmp[curr++] = arr[right++]; assignments++; comparisons++;
+	}
+	comparisons++;
 	for(int i = 0; i < size; i++, --end){
-		arr[end] = tmp[end];
+		arr[end] = tmp[end]; assignments++;  
 	}
 		
+}
+
+
+template <class T>
+void quicksort(T arr[], int size, int & assignments, int & comparisons){
+	assignments = 0;
+	comparisons = 0;
+	quicksort(arr,0,size-1,assignments, comparisons);	
+}
+
+
+
+template <class T>
+void quicksort(T arr[], int left, int right, int & assignments, int & comparisons){
+	if(left <  right){
+		const T& pivot = median3(arr,left,right,assignments,comparisons); assignments++;
+		int i = left, j = right-1; assignments += 2;
+		for(;;){
+			while(arr[++i] < pivot){comparisons++;}
+			while(pivot<arr[--j]){comparisons++;}
+			comparisons++;
+			if(i<j){
+				swap(arr[i], arr[j]); assignments+=3;
+			}
+			else
+				break;
+		}
+		
+		swap(arr[i],arr[right-1]); assignments += 3;
+		quicksort(arr,left,i-1,assignments,comparisons);
+		quicksort(arr,i+1,right,assignments,comparisons);
+	}
+}
+
+template <class T>
+const T & median3(T arr[], int left, int right, int & assignments, int & comparisons){
+	int center = (left + right)/2;  assignments++;
+	
+	comparisons++;
+	if(arr[center] < arr[left]){
+		swap(arr[left],arr[center]); assignments +=3;
+	}
+	comparisons++;
+	if(arr[right] < arr[left]){
+		swap(arr[left], arr[right]); assignments+=3;
+	}
+	comparisons++;
+	if(arr[right] < arr[center]){
+		swap(arr[center], arr[right]); assignments+=3;
+	}
+	
+	swap(arr[center], arr[right-1]); assignments+=3;
+	return arr[right-1];
+
+
 }
 
 
